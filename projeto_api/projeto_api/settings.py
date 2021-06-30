da08296a5a4730fde4d748923dcfdb5bc77b9ec0
Alfredo de Morais <alfredneto@hotmail.com>
2021-06-30 20:44:30 -0300

    seção 3

--- projeto_api/projeto_api/settings.py
+++ projeto_api/projeto_api/settings.py
@@ -133,6 +133,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticationOrReadOnly',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     )
 }
